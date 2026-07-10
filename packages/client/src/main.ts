cbc5c6cd92c17a0525f96c0b264edaaef1ee526d
Fayanzar <fayanzar@gmail.com>
2026-07-10 23:09:45 +0300

    add env file to vue

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -8,7 +8,7 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
-axios.defaults.baseURL='http://localhost:3000';
+axios.defaults.baseURL=import.meta.env.VITE_API_BASE_URL
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.withCredentials = true;
 
